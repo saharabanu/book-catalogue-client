77bfb2e2c8ba0dec426b0b387b00d848f036cee2
saharabanu <saharabanungn@gmail.com>
2023-07-14 15:16:21 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  
+  return (
+    <>
+      <h1>gghgjhjhk</h1>
+    </>
+  )
+}
+
+export default App;
+
+
+//http://wpdemo.magikthemes.com/fluence/
