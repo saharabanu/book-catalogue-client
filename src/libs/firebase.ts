4d0904cad305269ced61cfa9fc79112834535b04
saharabanu <saharabanungn@gmail.com>
2023-07-15 16:36:30 +0600

    [DONE] firebase Added

---
+++ src/libs/firebase.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+};
+console.log(firebaseConfig)
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
