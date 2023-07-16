c7843f9b54af06cd7b4470418dae7b6e8d0d9982
saharabanu <saharabanungn@gmail.com>
2023-07-16 15:50:28 +0600

    [done] user  login and logout  added

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
+import { useForm, SubmitHandler } from "react-hook-form"
+import Header from "../../layouts/Header"
+import { Link, useNavigate } from "react-router-dom"
+import { useAppDispatch } from "../../redux/hooks"
+import { loginUser } from "../../redux/features/user/userSlice"
+
+
+interface ILoginUser {
+  email:string,
+  password:string
+}
+
+const Login = () => {
+  
+  const { register, handleSubmit } = useForm<ILoginUser>();
+  
+  const navigate = useNavigate()
+  const dispatch =  useAppDispatch()
+  
+const onSubmit = (user:ILoginUser) =>{
+
+  console.log(user)
+  dispatch(loginUser(user))
+  .then(() => {
+    navigate('/');
+  })
+  .catch((error) => {
+    console.error('Login error:', error);
+  });
+
+}
+  // const { register, handleSubmit } = useForm<IFormInput>()
+  // const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+  return (
+    <>
+    <Header/>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      
+      <label>Email</label>
+      
+      <input {...register("email")} type="email" required placeholder="type Email"/>
+      <br /><br />
+      <label>Password</label>
+      <input {...register("password")} type="number" placeholder="enter password"/>
+      
+      
+      <input type="submit" />
+    </form>
+    <div>
+      <p> Don't have an Account? Please <Link to="/sign-up">Sign Up</Link></p>
+    </div>
+    
+    </>
+  )
+}
+
+export default Login
